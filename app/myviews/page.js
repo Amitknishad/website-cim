d6239703bc2a72f9fedf0c8d377f867f66c55a3c
Prafull Bind <prafullbind@gmail.com>
2024-10-18 15:00:12 +0530

    my views template

--- app/myviews/page.js
+++ app/myviews/page.js
@@ -3,15 +3,17 @@ import { Box } from '@mui/material';
 import Header from "@/components/piyush/Header";
 import Navbar from "@/components/piyush/Navbar";
 import Footer from "@/components/piyush/FooterSection";
-import FilterBar from '../../components/myviews/FilterBar';
+import FilterBar from '@/components/myviews/FilterBar';
+import TrendingArticles from "@/components/myviews/TrendingArticles";
 
 const myViews = () => {
   return (
     <Box>
-       <Header />
-       <Navbar />
-       <FilterBar />
-       <Footer />
+      <Header />
+      <Navbar />
+      <FilterBar />
+      <TrendingArticles />
+      <Footer />
     </Box>
   )
 }
